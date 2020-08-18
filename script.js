8165b7f4ffdb7e53402861634eaa524caa250444
Abdullahriyd <Abdullahriyd>
2020-08-18 21:49:08 +0600

    Final V1

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ document.getElementById('lyrics-show').style.display = 'none';
 document.querySelector('.search-total').style.display = 'none';
 
 document.getElementById('searchBtn').addEventListener('click', function(){
-    let searchText = document.getElementById('searchText').value;
+    const searchText = document.getElementById('searchText').value;
+    songSearch(searchText);
+})
+
+document.getElementById('searchText').addEventListener('change',function(){
+    const searchText = document.getElementById('searchText').value;
     songSearch(searchText);
 })
 
@@ -10,7 +15,7 @@ function songSearch (searchText) {
     fetch(`https://api.lyrics.ovh/suggest/${searchText}`) 
     .then(res => res.json())
     .then(data => displaySongs(data))
-    .catch(error => window.alert('No Data Found',error))
+    .catch(error => window.alert('No Result Found',error))
 }
 
 function displaySongs (data){
@@ -26,14 +31,7 @@ function displaySongs (data){
         const artistImage = element.artist.picture;
         const songDuration = element.duration;
         const songLink = element.link;
-    
-        // document.getElementById('song-name').innerText = songTitle;
-        // document.getElementById('author-name').innerText = artistName;
-        // document.getElementById('album-name').innerText = songAlbum;
-        // document.getElementById('author-image').src = `${artistImage}`;
-        // document.getElementById('song-time').innerHTML = (songDuration/60).toFixed(2) + ' min';
-        // document.getElementById('song-link').href = `${songLink}`;
-        showLyrics(artistName, songTitle);
+        
         songResultArea.innerHTML += `<div class="single-result row align-items-center my-3 p-3">
             <div class="col-md-9">
                 <h3 class="lyrics-name">${songTitle}</h3>
@@ -49,31 +47,40 @@ function displaySongs (data){
                 </div>
             </div>
             <div class="col-md-3 text-md-right text-center">
-                <button onclick="showLyrics('${artistName}','${songTitle}')" class="btn btn-success">Get Lyrics</button>
+            <button onclick="showLyrics('${artistName}', '${songTitle}')" class="btn btn-success">Get Lyrics</button>
             </div>
         </div>`;
 
      }
      document.querySelector('.search-total').style.display = 'block';
+     document.getElementById('searchText').value ='';
 }
 
-function showLyrics(artistName, songTitle){
-    fetch(`https://api.lyrics.ovh/v1/${artistName}/${songTitle}`) 
+function showLyrics(artist, title) {
+    fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`)
     .then(res => res.json())
-    .then(data => lyrics(data.lyrics))
-    .catch(error => window.alert('No Data Found',error))
+    .then(data => displayLyrics(data.lyrics, title, artist))
+    .catch(error => window.alert('No Lyrics Found',error))
+};
 
-    document.getElementById('lyrics-show').style.display = 'block';
+function displayLyrics(lyrics, songName, artistName) {
 
-    function lyrics(data){
-        const lyricArea = document.getElementById('lyrics-show');
-            lyricArea.innerHTML = '';
-            lyricArea.innerHTML += `<button class="btn go-back" >&lsaquo;</button>
-                                <h2 class="text-success mb-4">${songTitle}</h2>
-                                <pre class="lyric text-white">
-                                ${data}
-                                </pre>`;
-    }
-    
+            const oneLyrics = document.getElementById('lyrics-show');
+            oneLyrics.innerHTML = '';
+            oneLyrics.innerHTML += `
+                    <button onclick="back()" class="btn go-back">Back</button>
+                    <h2 class="text-success mb-4" style="margin-top: 40px;">${songName}</h2>
+                    <h3 class="text-warning mb-4" style="margin-top: 10px;">${artistName}</h3>
+                    <pre class="lyric text-white"> ${lyrics} </pre>`;
 
+            document.getElementById('lyrics-show').style.display = 'block';
+            document.querySelector('.search-total').style.display = 'none';
+            document.getElementById('search-area').style.display ='none'
 }
+
+function back (){
+    document.getElementById('lyrics-show').style.display = 'none';
+    document.querySelector('.search-total').style.display = 'block';
+    document.getElementById('search-area').style.display ='block'
+}
+
